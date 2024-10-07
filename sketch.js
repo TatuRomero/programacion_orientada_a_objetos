39c9436ca64eb6950f9d0a9f6c3a37456c465ef9
Tatu Romero <tatiana.romero@ucuenca.edu.ec>
2024-10-07 15:34:44 -0500

    Se crea un nuevo proyecto y se llena con código básico.

---
+++ sketch.js
@@ -0,0 +1,15 @@
+let posX, posY;
+let diametro;
+let radio;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  radio = ceil(random(5, 25));
+  diametro = radio * 2;
+  // console.log(diametro);
+}
+
+function draw() {
+  background(220);
+}
