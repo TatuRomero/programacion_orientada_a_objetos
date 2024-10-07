05ed7b76cfb9ac50569e835bb424dbdf88523919
Tatu Romero <tatiana.romero@ucuenca.edu.ec>
2024-10-07 16:56:25 -0500

    Se realizaron mejoras en el código.

--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ let posX, posY;
 let velX, velY;
 let diametro;
 let radio;
+let fondo;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  fondo = color(random(100, 255), random(100), random(255));
 
   radio = ceil(random(5, 50));
   diametro = radio * 2;
@@ -12,27 +14,29 @@ function setup() {
   posX = random(radio, width - radio);
   posY = random(radio, height - radio);
 
-  // velX =random(-10,10)
-  // velY =random(-10,10)
-  velX = 10;
-  velY = 0;
+  velX = random(-10, 10);
+  velY = random(-10, 10);
+  // velX=10
+  // velY=0
 }
 
 function draw() {
-  background(100);
+  background(fondo);
   fill(255);
   noStroke();
 
   posX += velX;
   posY += velY;
 
-  if (posX > width - radio) {
+  if (posX > width - radio || posX < radio) {
     velX *= -1;
+    fondo = color(random(100, 255), random(100), random(255));
     // console.log("se fué")
   }
 
-  if (posX < radio) {
-    velX *= -1;
+  if (posY > height - radio || posY < radio) {
+    velY *= -1;
+    fondo = color(random(100, 255), random(100), random(255));
   }
 
   circle(posX, posY, diametro);
